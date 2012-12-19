bac64ec9372050c888ef63e0c4362e8649debcd6
Frederick Ros <frederick.ros@gmail.com>
2012-12-19 07:06:14 +0100

    Started implementing as a separate lib

--- tasks/regarde.js
+++ tasks/regarde.js
@@ -7,6 +7,9 @@ module.exports = function (grunt) {
   grunt.registerTask('regarde', 'Observe files on the filesystem', function (target) {
     var name = this.name;
     var config = grunt.config(name);
+
+    new Regarde(grunt);
+
     var targets = target ? [target] : Object.keys(config);
     var done = this.async();
 
@@ -16,22 +19,44 @@ module.exports = function (grunt) {
       var name = t[0];
       var target = t[1];
       var pattern = target.files;
-      var options = {};
+      var options = {interval: 10};
 
-      var gaze = new Gaze(pattern, options, function (err, watcher) {
-        if (err) {
-          grunt.log.error(err.message);
-          return done();
-        }
-        // On changed/added/deleted
-        watcher.on('all', function (status, filepath) {
-          filepath = path.relative(process.cwd(), filepath);
-          grunt.event.emit('regarde:file:' + status, filepath);
-        });
-        // On watcher error
-        watcher.on('error', function (err) { grunt.log.error(err); });
+      // var gaze = new Gaze(pattern, options, function (err, watcher) {
+      //   if (err) {
+      //     grunt.log.error(err.message);
+      //     return done();
+      //   }
+
+      //   watcher.on('ready', function() {
+      //     console.log("FRED: ready received");
+      //     grunt.event.emit('regarde:init:' + name + ':done');
+      //   });
+
+      //   // On changed/added/deleted
+      //   watcher.on('all', function (status, filepath) {
+      //     console.log("FRED: detected : " + pattern);
+      //     filepath = path.relative(process.cwd(), filepath);
+      //     grunt.event.emit('regarde:file:' + status, filepath);
+      //   });
+      //   // On watcher error
+      //   watcher.on('error', function (err) { grunt.log.error(err); });
+
+      // });
+
+      var gaze = new Gaze(pattern, options);
+      gaze.on('ready', function (w) {
+        console.log("FRED: ready !!!");
         grunt.event.emit('regarde:init:' + name + ':done');
       });
+      gaze.on('error', function (err) { grunt.log.error(err); });
+      gaze.on('all', function (status, filepath) {
+          console.log("FRED: detected : " + pattern);
+          filepath = path.relative(process.cwd(), filepath);
+          grunt.event.emit('regarde:file:' + status, filepath);
+
+      });
+
+
     });
 
   });
